503546bec1d07f60d512baaa6b25b3f7f361b386
Hanzell <hanzellrivera95@email.com>
2024-01-13 20:24:23 -0600

    firts commit

---
+++ src/data/info.ts
@@ -0,0 +1,138 @@
+export const info = {
+  baseUrl: "https://hanzellrivera.dev",
+  name: "Hanzell Rivera",
+  jobDescription: "Full Stack Developer",
+  about: `Desarrollador Full Stack con 3 años de experiencia en el desarrollo de software empresarial, ofrezco una amplia gama de conocimientos
+  técnicos a cualquier proyecto. Mi habilidad abarca una variedad de tecnologías de vanguardia, incluyendo JavaScript, NodeJS, Express, ReactJS,
+  TypeScript, React Native, Dart, Flutter, Python, .Net Core, ASP.Net, SQL. Tengo un historial comprobado de entregar soluciones de software de
+  alta calidad. Me enorgullezco de mis habilidades excepcionales de comunicación y colaboración y siempre estoy dispuesto a aprender nuevas
+  tecnologías y metodologías. Sobre todo, estoy profundamente comprometido con el arte y la ciencia de la programación.
+  `,
+
+  experience: [
+    {
+      name: "ByondIT",
+      position: "Full Stack Developer",
+      location: "Panama City, Panama",
+      startDate: "Enero 2023",
+      endDate: "Diciembre 2023",
+      description: [
+        `Con la modalidad de outsourcing formé parte del equipo de desarrollo de la aplicación móvil Tigo Money de la empresa Millicom, trabajé en la
+        integración de funcionalidades relacionadas con paquetes/recargas y remesas. Tambien, en las integraciones de onboarding y login, Además,
+        fui parte del equipo de cashin y cashout, Mi principal responsabilidad consistió en crear nuevos features requeridos y asistencias a potenciales
+        bugs utilizando Flutter, Además, contribuí al desarrollo de API's utilizando Python para la consulta de remesas y facetec. Estas experiencias
+        me han permitido desempeñarme como full stack developer, trabajando tanto en el desarrollo frontend utilizando Flutter, como en la creación
+        de API's con Python para el backend. Estoy emocionado de seguir creciendo en el campo de desarrollo de aplicaciones móviles y de contribuir al
+        éxito de proyectos innovadores.`
+      ],
+    },
+
+    {
+      name: "Hypernovalabs",
+      position: "Back-end Developer",
+      location: "Panama City, Panama",
+      startDate: "Julio 2021",
+      endDate: "Diciembre 2022",
+      description: [
+        `Como Desarrollador Backend en Aludra, fui responsable del desarrollo de nuevos proyectos de integración y APIs. Utilizando mi experiencia
+        en C#, .NET Core y ASP.NET, dirigí con éxito la integración de sistemas externos como NAF, Ivend y Salesforce, reemplazando Event Grids con
+        tecnologías sin servidor como Azure Functions y Queues, y enviando correos electrónicos con SendGrid. Mis habilidades en NodeJS, caché de
+        Redis y procesos eficientes de carga de datos me ayudaron en el desarrollo de APIs y validaciones para obtener respuestas más rápidas en las
+        aplicaciones. Fui capaz de optimizar procesos masivos de carga de datos, integrando información de productos, precios e inventario con
+        sistemas externos. Además de mis habilidades técnicas, también mantuve y desarrollé nuevas funcionalidades en los servicios y módulos del
+        SaaS-PIM (Gestor de Información de Productos) de Aludra. Con un enfoque ágil (SCRUM) y fuertes habilidades de comunicación y
+        colaboración, aseguré la funcionalidad sin fisuras de todos los servicios, ofreciendo un desarrollo de software de alta calidad.`
+      ],
+    },
+
+    {
+      name: "Qordev",
+      position: "Back-end Developer",
+      location: "North Las Vegas, EEUU",
+      startDate: "Marzo 2021",
+      endDate: "Julio 2021",
+      description: [
+        `Formé parte del equipo encargado de desarrollar la aplicación móvil TypeMatch. La aplicación se enfoca en citas y conexiones, y fue
+        construida usando NodeJS, Express y MySQL. Mejoré la lógica de emparejamiento para aumentar la precisión de las parejas o individuos
+        sugeridos en función de las preferencias personales del usuario.• Mejoré la experiencia del usuario consumiendo la API de un CRM (AgilCRM)
+        cuando dos personas coincidían, iniciando una conversación, creando un nuevo perfil o actualizando a premium. Esto ayudó a enviar
+        información al usuario de manera más eficiente.• Además de la aplicación TypeMatch, también trabajé en el proyecto Kidlind, una aplicación
+        educativa para niños. Fui responsable del desarrollo de APIs en NodeJS, la conexión a APIs externas como MagicBlox para los recursos de libros,
+        y la gestión de la lógica de los libros.`
+      ],
+    },
+    {
+      name: "Nexitus",
+      position: "Front-end Developer",
+      location: "San José, Costa Rica",
+      startDate: "Enero 2021",
+      endDate: "Marzo 2021",
+      description: [
+        `Trabajé en el sitio web oficial de la empresa utilizando la tecnología NextJS para mostrar sus servicios y establecer su identidad en línea. El
+        framework NextJS me permitió crear un sitio web rápido, dinámico y fácil de usar que comunica eficazmente la misión y las ofertas de la
+        empresa.`
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Ingeniería en Sistemas de la Informacion",
+      location: "Nicaragua, Managua",
+      startDate: "2016",
+      endDate: "2021",
+      description: [
+        "Diplomado de Ingeniería en Sistemas de la Informacion",
+      ],
+    },
+    
+  ],
+
+  socialMedia: {
+    facebook: "https://www.facebook.com/",
+    twitter: "https://twitter.com/",
+    github: "https://github.com/RiveraHan",
+    email: "mailto:hanzellrivera95@gmail.com",
+    linkedin: "https://www.linkedin.com/in/hanzell-rivera/",
+  },
+
+  projects: [
+    {
+      title: "Buscador de Recetas",
+      isFeatured: true,
+      thumbnail: "/assets/images/bebidas.png",
+      githubUrl: "https://github.com/RiveraHan/react-bebidas",
+      liveUrl: "https://drinkrecipefinder.netlify.app/",
+    },{
+      title: "Buscador del Clima",
+      isFeatured: true,
+      thumbnail: "/assets/images/clima.png",
+      githubUrl: "https://github.com/RiveraHan/react-consulta-clima",
+      liveUrl: "https://searchsweather.netlify.app/",
+    },{
+      title: "Buscador de Letras de Canciones",
+      isFeatured: true,
+      thumbnail: "/assets/images/letras.png",
+      githubUrl: "https://github.com/RiveraHan/react-letras-cancion",
+      liveUrl: "https://searchsongs.netlify.app/",
+    },{
+      title: "E-commerce",
+      isFeatured: true,
+      thumbnail: "/assets/images/e-commerce.png",
+      githubUrl: "https://github.com/RiveraHan/carrito-nextjs-typescript-tailwind",
+      liveUrl: "https://e-commercehan.netlify.app/",
+    },,{
+      title: "Pokedex",
+      isFeatured: true,
+      thumbnail: "/assets/images/pokedex.png",
+      githubUrl: "https://github.com/RiveraHan/pokedex",
+      liveUrl: "https://github.com/RiveraHan/pokedex",
+    },,{
+      title: "TiktokClone",
+      isFeatured: true,
+      thumbnail: "/assets/images/tiktokClone.png",
+      githubUrl: "https://github.com/RiveraHan/tiktokClone",
+      liveUrl: "https://github.com/RiveraHan/tiktokClone",
+    },
+  ],
+};
