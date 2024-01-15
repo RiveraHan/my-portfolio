9a8e371e39a4a75f655d4c66baf124a86aa1088a
Hanzell <hanzellrivera95@email.com>
2024-01-15 09:59:36 -0600

    Fixed configuration vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,24 +2,21 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://hanzellrivera.vercel.app",
-  integrations: [
-    tailwind(),
-    sitemap({
-      changefreq: "weekly",
-      priority: 0.7,
-      lastmod: new Date(),
-      entryLimit: 10000,
-    }),
-    robotsTxt(),
-  ],
+  integrations: [tailwind(), sitemap({
+    changefreq: "weekly",
+    priority: 0.7,
+    lastmod: new Date(),
+    entryLimit: 10000
+  }), robotsTxt()],
   output: 'server',
   adapter: vercel({
     webAnalytics: {
-      enable: true
-    }
-  })
+      enabled: true,
+    },
+  }),
 });
