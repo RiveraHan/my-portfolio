dd102bf41bc46097739ea9d0331c14d2a93fac12
Hanzell Rivera <hanzellrivera95@email.com>
2024-03-10 00:15:25 -0600

    content was reduced

--- src/data/info.ts
+++ src/data/info.ts
@@ -2,10 +2,7 @@ export const info = {
   baseUrl: "https://hanzellrivera.vercel.app",
   name: "Hanzell Rivera",
   jobDescription: "Full Stack Developer",
-  about: `Ingeniero en Sistemas y Desarrollador Full Stack con 3 años de experiencia en software empresarial, ofrezco amplios conocimientos técnicos en 
-    JavaScript, NodeJS, Express, ReactJS, TypeScript, React Native, Dart, Flutter, Python, .Net Core, ASP.Net y SQL. 
-    Con historial probado de entregas de software de alta calidad, destaco por habilidades excepcionales de comunicación y colaboración. 
-    Siempre dispuesto a aprender nuevas tecnologías y metodologías, me comprometo profundamente con el arte y la ciencia de la programación.`,
+  about: `Ingeniero en Sistemas y Desarrollador Full Stack con 3 años de experiencia en software empresarial.`,
 
   experience: [
     {
@@ -15,10 +12,8 @@ export const info = {
       startDate: "Enero 2023",
       endDate: "Diciembre 2023",
       description: [
-        `En outsourcing, contribuí al desarrollo de la aplicación móvil Tigo Money de Millicom, centrado en paquetes/recargas, remesas, onboarding, login, cashin y cashout. 
-        Responsable de crear nuevos features y resolver bugs con Flutter. Además, aporté al desarrollo de API's en Python para remesas y facetec. 
-        Mi experiencia me ha formado como full stack developer, destacando en frontend (Flutter) y backend (Python). 
-        Emocionado por seguir creciendo en el desarrollo de aplicaciones móviles y contribuir al éxito de proyectos innovadores.`
+        `Con la modalidad de outsourcing, contribuí al desarrollo de la aplicación móvil Tigo Money de Millicom, en esta app tipo wallet fui parte de los equipos de paquetes/recargas (a números telefónicos), remesas, onboarding, login, cashin y cashout. 
+        Fui capaz de crear nuevos features y resolver bugs utilizando Flutter. Además, aporté al desarrollo de API's en Python para remesas`
       ],
     },
 
@@ -30,8 +25,7 @@ export const info = {
       endDate: "Diciembre 2022",
       description: [
         `Como Desarrollador Backend en Aludra, lideré proyectos de integración y APIs, utilizando C#, .NET Core, ASP.NET, NodeJS, Redis, Azure Functions, Queues y SendGrid. 
-        Gestioné con éxito la integración de sistemas externos como NAF, Ivend y Salesforce, optimizando procesos de carga de datos para respuestas más rápidas. 
-        Mantuve y desarrollé funcionalidades en el SaaS-PIM de Aludra, asegurando un desarrollo de software de alta calidad con enfoque ágil (SCRUM) y sólidas habilidades de comunicación.`
+        Gestioné con éxito la integración de sistemas externos como NAF, Ivend y Salesforce, optimizando procesos de carga de datos para respuestas más rápidas.`
       ],
     },
 
@@ -42,10 +36,9 @@ export const info = {
       startDate: "Marzo 2021",
       endDate: "Julio 2021",
       description: [
-        `Parte del equipo de desarrollo de la aplicación móvil TypeMatch, centrada en citas y conexiones. 
-        Construida con NodeJS, Express y MySQL, mejoré la lógica de emparejamiento para aumentar la precisión de las sugerencias. 
-        Optimicé la experiencia del usuario al consumir la API de un CRM (AgilCRM) para iniciar conversaciones y actualizar perfiles. 
-        Además, trabajé en Kidlind, una aplicación educativa, desarrollando APIs en NodeJS, conectando con MagicBlox para recursos de libros y gestionando la lógica de los libros.`
+        `Desarrolle en la aplicación móvil TypeMatch (centrada en citas y conexiones). 
+        Construida con NodeJS, Express y MySQL, mejoré la lógica de emparejamiento para aumentar la precisión de las sugerencias.  
+        Además, trabajé en Kidlind, una aplicación educativa, desarrollando APIs en NodeJS, gestión de recursos de libros.`
       ],
     },
     {
@@ -55,9 +48,7 @@ export const info = {
       startDate: "Enero 2021",
       endDate: "Marzo 2021",
       description: [
-        `Trabajé en el sitio web oficial de la empresa utilizando la tecnología NextJS para mostrar sus servicios y establecer su identidad en línea. El
-        framework NextJS me permitió crear un sitio web rápido, dinámico y fácil de usar que comunica eficazmente la misión y las ofertas de la
-        empresa.`
+        `Trabajé en el sitio web oficial de la empresa utilizando la tecnología NextJS para mostrar sus servicios y establecer su identidad en línea.`
       ],
     },
   ],
